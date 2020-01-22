031feb0db53d4473e2460367f379c9e84c840cff
Bryan Hazelbaker <bryan.hazelbaker@gmail.com>
2020-01-21 23:01:51 -0800

    Update ocsp_create_csr.sh

--- ocsp_create_csr.sh
+++ ocsp_create_csr.sh
@@ -1,38 +1,34 @@
 # Inspired by instructions from http://devcentral.f5.com/s/articles/building-an-openssl-certificate-authority-introduction-and-design-considertions-for-elliptical-curves-27720
-#
-# Switch to root before executing. To reset and start over first issue 'rm -Rf /root/ca'
-#
-# Execute on OCSP server to retain the secrecy of the ocsp private key
+# Switch to root before executing. To reset and start over first issue 'rm -Rf /root/ca/ocsp'.
+# Execute on OCSP responder host.
 
-#
 # Abort script if any error is encountered
 set -e
 
-#
 # Create directory structure
-if [ ! -d /root/ca ]; then
+if [ ! -d /root/ca/ocsp ]; then
 	printf "\n>> Generating /root/ca directory structure...\n"
-	mkdir /root/ca
-	mkdir /root/ca/private /root/ca/csr /root/ca/certs /root/ca/crl
+	mkdir -p /root/ca/ocsp/private /root/ca/ocsp/csr /root/ca/ocsp/certs
 fi
 
-#
+# Copy OpenSSL config file
+if [ ! -f /root/ca/openssl_root.cnf ]; then
+	cp openssl_root.cnf /root/ca/openssl_root.cnf
+fi
+
 # Generate root ocsp responder private key and encrypt
-if [ ! -f /root/ca/private/ocsp.guardtone.com.key.pem ]; then
+if [ ! -f /root/ca/ocsp/private/ocsp.guardtone.com.key.pem ]; then
 	printf "\n>> Generating root Certificate Authority (CA) private OCSP responder key and encrypting...\n\n"
-	openssl ecparam -genkey -name secp384r1 | openssl ec -aes256 -out /root/ca/private/ocsp.guardtone.com.key.pem
+	openssl ecparam -genkey -name secp384r1 | openssl ec -aes256 -out /root/ca/ocsp/private/ocsp.guardtone.com.key.pem
 fi
 
-#
 # Generate root ocsp responder Certificate Signing Request (CSR)
-if [ ! -f /root/ca/csr/ocsp.guardtone.com.csr ]; then
-	printf "\n>> Generating root OCSP responder Certificate Signing Request (CSR)...\n\n"
-	openssl req -config "openssl_root.cnf" -new -key "/root/ca/private/ocsp.guardtone.com.key.pem" -out "./ocsp.guardtone.com.csr"
+if [ ! -f /root/ca/ocsp/csr/ocsp.guardtone.com.csr ]; then
+	printf "\n>> Generating root CA OCSP responder Certificate Signing Request (CSR)...\n\n"
+	openssl req -config "/root/ca/openssl_root.cnf" -new -key "/root/ca/ocsp/private/ocsp.guardtone.com.key.pem" -out "/root/ca/ocsp/csr/ocsp.guardtone.com.csr"
 fi
 
-#
 # Print summary
 if [ -f /root/ca/private/ocsp.guardtone.com.key.pem ]; then
 	printf "\n\n>> New OCSP private key:\t/root/ca/private/ocsp.guardtone.crt.pem"
 fi
-
