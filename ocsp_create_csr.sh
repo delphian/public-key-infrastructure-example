78f382cccf0acc8ada1682f0dc1d74c4f53b88b0
Bryan Hazelbaker <bryan.hazelbaker@gmail.com>
2020-01-23 18:35:46 -0800

    Update ocsp_create_csr.sh

--- ocsp_create_csr.sh
+++ ocsp_create_csr.sh
@@ -1,34 +1,27 @@
 # Execute on OCSP responder host.
+DIR="/root/ca/ocsp"
+DOMAIN="guardtone"
 
 # Abort script if any error is encountered
 set -e
-
 # Create directory structure
-if [ ! -d /root/ca/ocsp ]; then
-	printf "\n>> Generating /root/ca directory structure...\n"
-	mkdir -p /root/ca/ocsp/private /root/ca/ocsp/csr /root/ca/ocsp/certs
+if [ ! -d ${DIR} ]; then
+	printf "\n>> Generating ${DIR} directory structure...\n"
+	mkdir -p ${DIR}/private ${DIR}/csr ${DIR}/certs
 fi
-
-# Copy OpenSSL config file
-if [ ! -f /root/ca/openssl_root.cnf ]; then
-	cp openssl_root.cnf /root/ca/openssl_root.cnf
-fi
-
 # Generate root ocsp responder private key and encrypt
-if [ ! -f /root/ca/ocsp/private/ocsp.guardtone.com.key.pem ]; then
+if [ ! -f ${DIR}/private/ocsp.${DOMAIN}.com.key.pem ]; then
 	printf "\n>> Generating root Certificate Authority (CA) private OCSP responder key and encrypting...\n\n"
-	openssl ecparam -genkey -name secp384r1 | openssl ec -aes256 -out /root/ca/ocsp/private/ocsp.guardtone.com.key.pem
+	openssl ecparam -genkey -name secp384r1 | openssl ec -aes256 -out ${DIR}/private/ocsp.${DOMAIN}.com.key.pem
 fi
-
 # Generate root ocsp responder Certificate Signing Request (CSR)
-if [ ! -f /root/ca/ocsp/csr/ocsp.guardtone.com.csr ]; then
+if [ ! -f ${DIR}/csr/ocsp.${DOMAIN}.com.csr ]; then
 	printf "\n>> Generating root CA OCSP responder Certificate Signing Request (CSR)...\n"
-	printf "!! Common Name should be: ocsp.guardtone.com !!\n\n"
-	openssl req -config "/root/ca/openssl_root.cnf" -new -key "/root/ca/ocsp/private/ocsp.guardtone.com.key.pem" -out "/root/ca/ocsp/csr/ocsp.guardtone.com.csr" -extensions "server_cert"
+	printf "!! Common Name should be: ocsp.${DOMAIN}.com !!\n\n"
+	openssl req -config "./openssl_root.cnf" -new -key "${DIR}/private/ocsp.${DOMAIN}.com.key.pem" -out "${DIR}/csr/ocsp.${DOMAIN}.com.csr" -extensions "server_cert"
 fi
-
 # Print summary
-if [ -f /root/ca/private/ocsp.guardtone.com.key.pem ]; then
-	printf "\n\n>> New OCSP private key:\t\t/root/ca/ocsp/private/ocsp.guardtone.crt.pem"
-	printf "\n>> New OCSP certificate request:\t/root/ca/ocsp/csr/ocsp.guardtone.com.csr"
+if [ -f ${DIR}/private/ocsp.${DOMAIN}.com.key.pem ]; then
+	printf "\n\n>> New OCSP private key:\t\t${DIR}/private/ocsp.${DOMAIN}.crt.pem"
+	printf "\n>> New OCSP certificate request:\t${DIR}/csr/ocsp.${DOMAIN}.com.csr"
 fi
