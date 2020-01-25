5a0d5a2193c80bbb41865d5331784aec0cea93df
Bryan Hazelbaker <bryan.hazelbaker@gmail.com>
2020-01-24 18:11:10 -0800

    Update root_ca_sign_ocsp_csr.sh

--- root_ca_sign_ocsp_csr.sh
+++ root_ca_sign_ocsp_csr.sh
@@ -1,31 +1,25 @@
-# Inspired by https://devcentral.f5.com/s/articles/building-an-openssl-certificate-authority-introduction-and-design-considertions-for-elliptical-curves-27720
-# Switch to root before executing
-# Place the OCSP responder Certificate Signing Request (CSR) in /root/ca/csr
+# Sign a OCSP responder Certificate Signing Request (CSR)
+DIR="/root/ca"
 
 # Abort script if any error is encountered
 set -e
-
 # Enumerate potential Certificate Signing Requests (CSR)
-ls /root/ca/csr
-
+ls "{$DIR}/csr"
 # Choose OCSP CSR to sign
 printf "\nEnter file name of OCSP Certificate Signing Request (CSR) to sign. Ommit .csr extension: "
 read DOMAIN
-
 # Sign OCSP Certificate Signing Request (CSR)
-if [ -f "/root/ca/csr/${DOMAIN}.csr" ]; then
+if [ -f "${DIR}/csr/${DOMAIN}.csr" ]; then
 	printf "\n>> Signing ${DOMAIN}.csr...\n\n"
-	openssl ca -config "/root/ca/openssl_root.cnf" -extensions ocsp -days 3650 -md sha384 -in "/root/ca/csr/${DOMAIN}.csr" -out "/root/ca/certs/${DOMAIN}.crt.pem"
+	openssl ca -config "./openssl_root.cnf" -extensions ocsp -days 3650 -md sha384 -in "${DIR}/csr/${DOMAIN}.csr" -out "${DIR}/certs/${DOMAIN}.crt.pem"
 fi
-
 # Validate OCSP responder certificate
-if [ -f "/root/ca/certs/${DOMAIN}.crt.pem" ]; then
+if [ -f "${DIR}/certs/${DOMAIN}.crt.pem" ]; then
 	printf "\n>> Validating OCSP responder certificate...\n\n"
-	openssl x509 -noout -text -in "/root/ca/certs/${DOMAIN}.crt.pem"
+	openssl x509 -noout -text -in "${DIR}/certs/${DOMAIN}.crt.pem"
 fi
-
 # Summary
-if [ -f "/root/ca/certs/${DOMAIN}.crt.pem" ]; then
-	printf "\n>> Signed OCSP certificate: /root/ca/certs/${DOMAIN}.crt.pem\n\n"
+if [ -f "${DIR}/certs/${DOMAIN}.crt.pem" ]; then
+	printf "\n>> Signed OCSP certificate: ${DIR}/certs/${DOMAIN}.crt.pem\n\n"
 	printf ">> Please copy the above certificate back to the OCSP server\n\n"
 fi
