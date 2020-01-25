812a5e92b78421e4a9a304c1b7554738c061f349
Bryan Hazelbaker <bryan.hazelbaker@gmail.com>
2020-01-25 00:36:22 -0800

    Update root_ca_update_crl.sh

--- root_ca_update_crl.sh
+++ root_ca_update_crl.sh
@@ -1,12 +1,8 @@
-# Inspired by instructions from http://devcentral.f5.com/s/articles/building-an-openssl-certificate-authority-introduction-and-design-considertions-for-elliptical-curves-27720
-#
-# Switch to root before executing
-#
+DIR="/root/ca"
+
 # Abort script if any error is encountered
 set -e
-#
 # Update certificate revocation list
-openssl ca -config /root/ca/openssl_root.cnf -gencrl -out /root/ca/crl/revoked.crl
-#
+openssl ca -config "./root_ca_openssl.cnf" -gencrl -out "${DIR}/crl/revoked.crl"
 # Verify certificate revocation list
-openssl crl -in /root/ca/crl/revoked.crl -noout -text
+openssl crl -in "${DIR}/crl/revoked.crl" -noout -text
