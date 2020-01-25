bd0896cb5a0d1f20e34750906ee3a47c59839c2b
Bryan Hazelbaker <bryan.hazelbaker@gmail.com>
2020-01-25 13:32:21 -0800

    Update intermediate_ca_initialize.sh

--- intermediate_ca_initialize.sh
+++ intermediate_ca_initialize.sh
@@ -1,33 +1,40 @@
-DIR="/root/ca/intermediate"
-DOMAIN="guardtone"
-CA="ca.home"
+# Initialize a Certificate Authority
+DIR="/root/ca"
+CERT_URL="ca.home.guardtone.com"
+CERT_NAME="Home GuardTone Certificate Authority"
+CONFIG="./intermediate_ca_openssl.cnf"
 
 # Abort script if any error is encountered
 set -e
 # Create directory structure
 if [ ! -d "${DIR}" ]; then
-	printf "\n>> Generating intermediate Certificate Authority (CA) directory structure...\n"
-	mkdir -p "${DIR}/private" "${DIR}/certs" "${DIR}/crl"
+	printf "\n>> Generating ${DIR} directory structure...\n"
+	mkdir -p "${DIR}/private" "${DIR}/csr" "${DIR}/certs" "${DIR}/crl"
 	touch "${DIR}/index.txt"
 	echo 1000 > "${DIR}/serial"
 	echo 1000 > "${DIR}/crlnumber"
 fi
-# Generate intermediate private key and encrypt
-if [ ! -f "${DIR}/private/${CA}.${DOMAIN}.key.pem" ]; then
-	printf "\n>> Generating intermediate CA private key and encrypting...\n\n"
-	openssl ecparam -genkey -name secp384r1 | openssl ec -aes256 -out "${DIR}/private/${CA}.${DOMAIN}.key.pem"
+# Generate private key and encrypt
+if [ ! -f "${DIR}/private/${CERT_URL}.key.pem" ]; then
+	printf "\n>> Generating ${CERT_NAME} private key and encrypting...\n\n"
+	openssl ecparam -genkey -name secp384r1 | openssl ec -aes256 -out "${DIR}/private/${CERT_URL}.key.pem"
 fi
-# Generate intermediate Certificate Signing Request (CSR)
-if [ ! -f "${DIR}/csr/${CA}.${DOMAIN}.csr" ]; then
-	printf "\n>> Generating intermediate CA Certificate Signing Request (CSR) for root CA...\n\n"
-	openssl req -config "./intermediate_ca_openssl.cnf" -new -key "${DIR}/private/${CA}.${DOMAIN}.key.pem" -out "${DIR}/csr/${CA}.${DOMAIN}.csr"
+# Generate Certificate Signing Request (CSR)
+if [ ! -f "${DIR}/certs/${CERT_URL}.crt.pem" ]; then
+	printf "\n>> Generating ${CERT_NAME} Certificate Signing Request (CSR)...\n"
+	printf "!! Common Name should be: ${CERT_NAME} !!\n\n"
+	openssl req -config "${CONFIG}" \
+	            -new -sha384 \
+		    -key "${DIR}/private/${CERT_URL}.key.pem" \
+		    -out "${DIR}/csr/${CERT_URL}.crs"
 fi
-# Generate intermediate certificate revocation list
-if [ ! -f "${DIR}/crl/${CA}-revoked.crl" ]; then
-	printf "\n>> Generating intermediate CA Certificate Revocation List (CRL)"
-	openssl ca -config "./intermediate_ca_openssl.cnf" -gencrl -out "${DIR}/crl/${CA}-revoked.crl"
+# Create Certificate Revocation List
+if [ ! -f "${DIR}/crl/revoked.crl" ]; then
+	printf "\n>> Generating ${CERT_NAME} Certificate Revocation List (CRL)...\n\n"
+	openssl ca -config "${CONFIG}" -gencrl -out "${DIR}/crl/revoked.crl"
 fi
 # Summary
-if [ -f "${DIR}/csr/home.ca.${DOMAIN}.csr" ]; then
-	printf "\n\n>> Intermediate Certificate Signing Request (CSR): ${DIR}/csr/${CA}.${DOMAIN}.csr\n\n"
+if [ -f "${DIR}/certs/${CERT_DOMAIN}.crt.pem" ]; then
+	printf "\n\n>> New ${CERT_NAME} Certificate Signing Request (CSR):\t${DIR}/certs/${CERT_URL}.csr"
+	printf "\n>> New ${CERT_NAME} Certificate Revocation List (CRL):\t\t${DIR}/crl/revoked.crl\n\n"
 fi
