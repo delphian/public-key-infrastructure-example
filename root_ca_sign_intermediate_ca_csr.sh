ac67ba8e9e5256c8c47da8b45f1a9ae06cba4e8b
Bryan Hazelbaker <bryan.hazelbaker@gmail.com>
2020-01-25 00:41:56 -0800

    Update root_ca_sign_intermediate_ca_csr.sh

--- root_ca_sign_intermediate_ca_csr.sh
+++ root_ca_sign_intermediate_ca_csr.sh
@@ -1,40 +1,25 @@
-# Inspired by instructions from http://devcentral.f5.com/s/articles/building-an-openssl-certificate-authority-introduction-and-design-considertions-for-elliptical-curves-27720
-#
-# Switch to root before executing
-#
-# Place the Certificate Signing Request in /root/ca/intermediate/csr/
+# Place the Certificate Signing Request in /root/ca/csr/
+DIR="/root/ca"
 
-#
 # Abort script if any error is encountered
 set -e
-
-#
 # Enumerate potential Certificate Signing Requests (CSR)
-ls /root/ca/csr
-
-#
+ls "${DIR}/csr"
 # Choose CSR to sign
-printf "\nEnter file name of Certificate Signing Request (CSR) to sign. Ommit .csr extension: "
+printf "\nEnter file name of Certificate Signing Request (CSR) to sign. Omit .csr extension: "
 read DOMAIN
-
-#
 # Sign intermediate Certificate Signing Request
-if [ -f "/root/ca/csr/${DOMAIN}.csr" ]; then
+if [ -f "${DIR}/csr/${DOMAIN}.csr" ]; then
 	printf "\n>> Signing ${DOMAIN}.csr...\n\n"
-	openssl ca -config "/root/ca/openssl_root.cnf" -extensions v3_intermediate_ca -days 3650 -md sha384 -in "/root/ca/csr/${DOMAIN}.csr" -out "/root/ca/certs/${DOMAIN}.crt.pem"
+	openssl ca -config "./root_ca_openssl.cnf" -extensions v3_intermediate_ca -days 3650 -md sha384 -in "${DIR}/csr/${DOMAIN}.csr" -out "${DIR}/certs/${DOMAIN}.crt.pem"
 fi
-
-#
 # Validate intermediate certificate contents
-if [ -f "/root/ca/certs/${DOMAIN}.crt.pem" ]; then
+if [ -f "${DIR}/certs/${DOMAIN}.crt.pem" ]; then
 	printf "\n>> Validating intermediate CA Certificate...\n\n"
-	openssl x509 -noout -text -in "/root/ca/certs/${DOMAIN}.crt.pem"
+	openssl x509 -noout -text -in "${DIR}/certs/${DOMAIN}.crt.pem"
 fi
-
-#
 # Summary
-if [ -f "/root/ca/certs/${DOMAIN}.crt.pem" ]; then
-	printf "\n>> Signed intermediate CA Certificate: /root/ca/certs/${DOMAIN}.crt.pem\n\n"
-	printf ">> Copy signed certificate back to intermediate Certificate Authority (probably /root/ca/intermediate/certs/\n\n"
+if [ -f "${DIR}/certs/${DOMAIN}.crt.pem" ]; then
+	printf "\n>> Signed intermediate CA Certificate: ${DIR}/certs/${DOMAIN}.crt.pem\n\n"
+	printf ">> Copy signed certificate back to intermediate Certificate Authority\n\n"
 fi
-
