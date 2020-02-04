ca94454d2830e774ebede94c3d015fb0741f49c4
Bryan Hazelbaker <bryan.hazelbaker@gmail.com>
2020-02-04 00:18:51 -0800

    Delete root_ca_update_crl.sh

--- root_ca_update_crl.sh
+++
@@ -1,8 +0,0 @@
-DIR="/root/ca"
-
-# Abort script if any error is encountered
-set -e
-# Update certificate revocation list
-openssl ca -config "./root_ca_openssl.cnf" -gencrl -out "${DIR}/crl/revoked.crl"
-# Verify certificate revocation list
-openssl crl -in "${DIR}/crl/revoked.crl" -noout -text
