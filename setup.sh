d61c1b17a8caddc7e07eb2b50f8b380d98e8cfbc
Bryan Hazelbaker <bryan.hazelbaker@gmail.com>
2020-02-04 17:42:28 -0800

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,7 @@
+$IP_CA_OFFLINE = "192.168.10.230"
+$IP_CA = "192.168.10.229"
+$IP_
+
+echo "192.168.10.230  ca-offline.guardtone.com" >> /etc/hosts
+echo "192.168.10.229  ca.guardtone.com" >> /etc/hosts
+echo "192.168.10.228  ca-public.guardtone.com" >> /etc/hosts
